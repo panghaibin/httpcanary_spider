8b9e4c02e5ebabd9e0d21c2eddd675e349fd1f1a
panghaibin <panghaibin@shu.edu.cn>
2022-03-25 00:23:01 +0800

    2

---
+++ parse.py
@@ -0,0 +1,213 @@
+import os
+import re
+import json
+import socket
+import urllib3
+import logging
+from io import BytesIO
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+from http.client import HTTPResponse
+from http.server import BaseHTTPRequestHandler
+
+
+class BytesIOSocket(socket.socket):
+    def __init__(self, content):
+        super().__init__()
+        self.handle = BytesIO(content)
+
+    def makefile(self, *args, **kwargs):
+        return self.handle
+
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, data):
+        self.rfile = BytesIO(data)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+
+    def send_error(self, code, message=None, explain=None):
+        self.error_code = code
+        self.error_message = message
+
+
+class ParseResponse:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        logging.info('[+] Reading file: {}'.format(file_path))
+        with open(file_path, 'rb') as f:
+            self.file_content = f.read()
+        self.response = self._response_from_bytes(self.file_content)
+
+    @staticmethod
+    def _response_from_bytes(data):
+        sock = BytesIOSocket(data)
+        response = HTTPResponse(sock)
+        response.begin()
+        return urllib3.HTTPResponse.from_httplib(response)
+
+    def get_file_content(self):
+        return self.file_content
+
+    def get_response(self):
+        return self.response
+
+    def get_json(self):
+        return json.loads(self.response.data.decode('utf-8'))
+
+    def get_urlparse(self):
+        return parse_qs(urlparse(self.response.data.decode('utf-8')).query)
+
+    def print_json(self):
+        print(json.dumps(self.get_json(), indent=4, ensure_ascii=False))
+
+    def print_urlparse(self):
+        print(json.dumps(self.get_urlparse(), indent=4, ensure_ascii=False))
+
+    def print_readable(self):
+        try:
+            self.print_json()
+        except json.JSONDecodeError:
+            self.print_urlparse()
+
+
+class ParseRequest:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        with open(file_path, 'rb') as f:
+            self.file_content = f.read()
+        self.request = self._request_from_bytes(self.file_content)
+
+    @staticmethod
+    def _request_from_bytes(data):
+        return HTTPRequest(data)
+
+    def check_err(self):
+        if self.request.error_code is not None:
+            return False
+        return True
+
+    def get_file_content(self):
+        return self.file_content
+
+    def get_request(self):
+        return self.request
+
+    def get_host(self):
+        return self.request.headers['host']
+
+    def get_url(self):
+        return self.request.path
+
+    def get_urlparse(self):
+        return parse_qs(urlparse(self.request.path).query)
+
+    def get_json(self):
+        raw_data = self.request.rfile.read()
+        try:
+            return json.loads(raw_data.decode('utf-8'))
+        except json.JSONDecodeError:
+            return None
+
+    def print_urlparse(self):
+        try:
+            print(json.dumps(self.get_urlparse(), indent=4, ensure_ascii=False))
+        except Exception as e:
+            logging.info('[-] Error: {} {}'.format(self.file_path, e))
+
+    def print_json(self):
+        try:
+            print(json.dumps(self.get_json(), indent=4, ensure_ascii=False))
+        except Exception as e:
+            logging.info('[-] Error: {} {}'.format(self.file_path, e))
+
+    def print_readable(self):
+        self.print_urlparse()
+        self.print_json()
+
+    def check_host(self, regx):
+        if re.search(regx, self.get_host()):
+            return True
+        return False
+
+
+class ParseFile:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.req_path = None
+        self.res_path = None
+        self.type = self.check_type()
+        self._parse_file_path()
+        self.request = ParseRequest(self.req_path)
+        self.response = ParseResponse(self.res_path)
+
+    def check_type(self):
+        if self.file_path.find('http_req') != -1:
+            return 'req'
+        elif self.file_path.find('http_res') != -1:
+            return 'res'
+        else:
+            return 'unknown'
+
+    def _parse_file_path(self):
+        if self.type == 'req':
+            self.req_path = self.file_path
+            self.res_path = self.file_path.replace('http_req', 'http_res')
+        elif self.type == 'res':
+            self.req_path = self.file_path.replace('http_res', 'http_req')
+            self.res_path = self.file_path
+        else:
+            logging.info('[-] Error: {}'.format(self.file_path))
+        return
+
+
+class ParseDir:
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        self.files = self.get_files()
+        self.parse_files = self.get_parse_files()
+
+    def get_files(self):
+        files = []
+        for file in os.listdir(self.dir_path):
+            if file.startswith('http_req'):
+                files.append(file)
+        return files
+
+    def get_parse_files(self):
+        parse_files = []
+        for file in self.files:
+            file_path = os.path.join(self.dir_path, file)
+            try:
+                parse_file = ParseFile(file_path)
+                parse_files.append(parse_file)
+            except Exception as e:
+                logging.info('[-] Error: {} - {}'.format(file_path, e), end='')
+                continue
+        return parse_files
+
+    def filter_req_host(self, regx):
+        parse_files = []
+        for parse_file in self.parse_files:
+            if parse_file.request.check_host(regx):
+                parse_files.append(parse_file)
+        return parse_files
+
+
+if __name__ == '__main__':
+    file_dir = r'E:\Python\httpcanary\upload\2022-03-24_22-38-24'
+    parse_dir = ParseDir(file_dir)
+    files = parse_dir.filter_req_host('dianping')
+    print(len(files))
+    for file in files:
+        print(file.file_path)
+        print('request:')
+        file.request.print_readable()
+        # print('response:')
+        # file.response.print_readable()
+        # break
+        print('=' * 10)
+
+    # file_path = r'E:\Python\httpcanary\upload\2022-03-24_22-38-24\http_res_02acd125-6a6f-4788-a3a7-c9903b21f3e7.hcy'
+    # parse_file = ParseResponse(file_path)
